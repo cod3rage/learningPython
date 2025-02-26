9298a3101f66d6124a9f661ef4c280c7f9163d13
cod3_rage <arielh9001@gmail.com>
2025-02-25 20:11:57 -0800

    Added button hitbox and functionality, Save and load files function too.

--- pygames/pixelArt/main.py
+++ pygames/pixelArt/main.py
@@ -1,5 +1,6 @@
+import json
 import pygame
-from math import floor
+
     
 class pixel_drawer:
   def __init__(self):
@@ -12,11 +13,32 @@ class pixel_drawer:
     self.grid_surface = pygame.surface.Surface((480,480))
     self.grid_surface = self.grid_surface.convert_alpha()
 
-    self.grid_background = pygame.surface.Surface((480,480))
     self.grid = {}
+
+    self.grid_background = pygame.surface.Surface((480,480))
 
     self.color0 = (255,0,0)
     self.color1 = (0,0,255)
+
+   
+
+    self.buttons = [
+      {'pos':15,'signal':'clear'},
+      
+      {'pos':105,'signal':'orange','color':(255,165,0)},
+      {'pos':135,'signal':'red','color':(255,0,0)},
+      {'pos':165,'signal':'violet','color':(238,130,238)},
+      {'pos':195,'signal':'blue','color':(0,0,255)},
+      {'pos':225,'signal':'yellow','color':(255,255,0)},
+      {'pos':255,'signal':'green','color':(0,255,0)},
+      {'pos':285,'signal':'indigo','color':(75,0,130)},
+      {'pos':315,'signal':'black','color':(0,0,0)},
+      {'pos':345,'signal':'white','color':(255,255,255)},
+
+      {'pos':445,'signal':'save'},
+      {'pos':470,'signal':'load'},
+    ]
+
   
   def run(self):
     self.render()
@@ -43,6 +65,8 @@ class pixel_drawer:
     self.grid_surface.fill(pygame.color.Color(0,0,0,0))
     for val in self.grid.values():
       pygame.draw.rect(self.grid_surface,val[1],val[0])
+    for things in self.buttons:
+      pygame.draw.rect(self.screen, (255,255,255),pygame.rect.Rect(9,things['pos'],124,20))
     self.screen.blit(self.grid_surface,(144,10))
   
   def inputs(self):
@@ -51,6 +75,36 @@ class pixel_drawer:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         self.running = False
+      if (pygame.MOUSEBUTTONUP == event.type or event.type == pygame.MOUSEBUTTONDOWN) and (event.button==1 or event.button==3) and (9<=mouse_pos[0]<=133):
+        for button in self.buttons:
+
+          if  button['pos'] <= mouse_pos[1] <= button['pos'] + 20:
+            if 'color' in button:
+              if event.button==1:
+                self.color0 = button['color']
+              else:
+                self.color1 = button['color']
+
+            elif button['signal'] == 'clear':
+              self.grid = {}
+
+            elif button['signal'] == 'save':
+              savefile = {}
+
+              for i,v in self.grid.items():
+                savefile[i]=v[1]
+
+              with open('saves/image_data.json','w') as jsonSave:
+                json.dump(savefile,jsonSave)
+              pygame.image.save(self.grid_surface,'saves/image.png')
+
+            elif button['signal'] == 'load':
+              self.grid = {}
+              with open('saves/image_data.json','r') as jsonSave:
+                for pos,color in json.load(jsonSave).items():
+                  x, y = pos.split(':')
+                  self.grid[pos] = [pygame.rect.Rect(int(x)*30,int(y)*30,30,30),tuple(color)]
+
 
     if 144 <= mouse_pos[0] <= 624 and  10 <= mouse_pos[1] <= 490:
       mouse_pressed = pygame.mouse.get_pressed()
@@ -63,7 +117,7 @@ class pixel_drawer:
     pass
 
   def draw(self,mouse_pos=(0,0),color=(255,255,255)):
-    base_x,base_y = floor((mouse_pos[0] - 144)/30)  ,  floor((mouse_pos[1] - 10)/30)
+    base_x,base_y = (mouse_pos[0] - 144)//30  ,  (mouse_pos[1] - 10)//30
     chunk = str(base_x)+':'+str(base_y)
     if chunk in self.grid:
       chuck_color = self.grid[chunk][1]
@@ -76,7 +130,7 @@ class pixel_drawer:
     if chunk and mouse_pos in self.grid:
       self.grid.pop(mouse_pos)
       return  
-    chunk = str(floor((mouse_pos[0] - 144)/30))+':'+str(floor((mouse_pos[1] - 10)/30))
+    chunk = str((mouse_pos[0] - 144)//30)+':'+str((mouse_pos[1] - 10)//30)
     if chunk in self.grid: self.erase(chunk,True)
 
 
